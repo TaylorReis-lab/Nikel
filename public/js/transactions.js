c3ed17e58d45194fe7cc4343e05035cee9edcadc
Edmilson Taylor Dos Reis <reist98822@gmail.com>
2022-12-28 13:50:25 -0300

    Inciando o git

---
+++ public/js/transactions.js
@@ -0,0 +1,96 @@
+const myModal = new bootstrap.Modal("#transaction-modal");
+let logged = sessionStorage.getItem("logged");
+const session = localStorage.getItem("session");
+let data = {
+    transaction: []
+}
+
+document.getElementById("button-logout").addEventListener("click", logout); 
+
+
+// ADICIONAR LAÇAMENTO
+
+document.getElementById("transaction-form").addEventListener("submit", function(e){
+    e.preventDefault();
+
+    const value = parseFloat(document.getElementById("value-input").value);
+    const description = document.getElementById("description-input").value;
+    const date = document.getElementById("date-input").value;
+    const type = document.querySelector('input[name="type-input"]:checked').value;
+
+    data.transaction.unshift({
+        value: value, type: type, description: description, date: date
+    });
+
+    saveData(data);
+    e.target.reset();
+    myModal.hide();
+
+    getTransactions();
+
+    alert("Lançamento feito com sucesso.");
+
+});
+
+// CHECAR SE ESTA LOGADO
+
+checkLogged();
+
+function checkLogged() {    
+    if(session) {
+        sessionStorage.setItem("logged", session)
+        logged = session;
+    }
+
+    if(!logged) {
+        window.location.href = "index.html";
+        return;
+    }
+
+    const dataUser = localStorage.getItem(logged);
+    if(dataUser) {
+        data = JSON.parse(dataUser);
+    }
+
+    getTransactions();
+
+}
+
+// DESLOG
+
+function logout() {
+    sessionStorage.removeItem("logged");
+    localStorage.removeItem("session");
+
+    window.location.href = "index.html";
+}
+
+function getTransactions() {
+    const transactions = data.transaction;
+    let transactionsHtml = ``;
+
+    if(transactions.length) {
+        transactions.forEach ((item) => {
+            let type = "Entrada";
+
+            if(item.type === "2") {
+                type = "Saida"
+            }
+
+            transactionsHtml += `
+                <tr>
+                    <th scope="row">${item.date}</th>
+                    <td>${item.value.toFixed(2)}</td>
+                    <td>${type}</td>
+                    <td>${item.description}</td>
+                 </tr>
+            `
+        })
+    }
+
+    document.getElementById("transactions-list").innerHTML = transactionsHtml;
+}
+
+function saveData(data) {
+    localStorage.setItem(data.login, JSON.stringify(data));
+}
